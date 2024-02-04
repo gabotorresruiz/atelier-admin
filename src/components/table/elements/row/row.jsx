aed139711ee385aaf89e706bdb509270a94a45f4
yuligomez <yulianagomezsilva@gmail.com>
2024-02-04 17:04:14 -0300

    change price in columns

--- src/components/table/elements/row/row.jsx
+++ src/components/table/elements/row/row.jsx
@@ -91,14 +91,14 @@ const Row = ({ data, headColumns, index, selected, setSelected }) => {
               </StyledImageCell>
             );
 
-          case 'numeric':
-            const formatter = new Intl.NumberFormat('de-DE', {
+          case 'number':
+            const formatter = new Intl.NumberFormat('es-UY', {
               style: 'currency',
-              currency: 'USD',
+              currency: 'UYU',
             });
 
             return (
-              <StyledNumericTableCell key={item.id} align='right'>
+              <StyledNumericTableCell key={item.id}>
                 {formatter.format(data[item.id])}
               </StyledNumericTableCell>
             );
